0126d00bbafb50307f9a44c343aca7fb99bbb752
onesix1219 <onesix1219@gmail.com>
2026-05-08 00:53:54 +0900

    Add files via upload

--- api/notion.js
+++ api/notion.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
     const rows = data.results.map(page => {
       const props = page.properties;
       return {
+        // 기본 정보
         학교: props['학교']?.title?.[0]?.plain_text || '',
         과목: props['과목']?.select?.name || '',
         세부과목: props['세부과목']?.select?.name || '',
@@ -32,11 +33,29 @@ export default async function handler(req, res) {
         학년: props['학년']?.select?.name || '',
         학기: props['학기']?.select?.name || '',
         '중간/기말': props['중간/기말']?.select?.name || '',
+        // 등급컷
         '1등급컷': props['1등급컷']?.number ?? null,
         '2등급컷': props['2등급컷']?.number ?? null,
+        // 링크
         '분석지 이미지 링크': props['분석지 이미지 링크']?.url || '',
         '기출 분석지 원본 링크': props['기출 분석지 원본 링크']?.url || '',
         '시험지 원본 링크': props['시험지 원본 링크']?.url || '',
+        // 문제 유형
+        '객관식': props['객관식']?.number || 0,
+        '서술형': props['서술형']?.number || 0,
+        '단답형': props['단답형']?.number || 0,
+        // 난이도
+        '난이도_하': props['난이도_하']?.number || 0,
+        '난이도_중하': props['난이도_중하']?.number || 0,
+        '난이도_중': props['난이도_중']?.number || 0,
+        '난이도_중상': props['난이도_중상']?.number || 0,
+        '난이도_상': props['난이도_상']?.number || 0,
+        '난이도_최상': props['난이도_최상']?.number || 0,
+        // 연계 출처
+        '연계_교과서': props['연계_교과서']?.number || 0,
+        '연계_학교프린트': props['연계_학교프린트']?.number || 0,
+        '연계_부교재': props['연계_부교재']?.number || 0,
+        '연계_비연계': props['연계_비연계']?.number || 0,
       };
     });
 
